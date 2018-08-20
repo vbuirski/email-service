5ef25a90a48fbb677b36d66d1ade7f364a8ccac6
vbuirski <vonita.buirski@gmail.com>
2018-08-20 21:27:09 +1000

    Removed redundant ObjectMapper as this is provided by SpringBoot.

--- src/main/java/au/com/vb/springboot/controller/EmailController.java
+++ src/main/java/au/com/vb/springboot/controller/EmailController.java
@@ -2,13 +2,9 @@ package au.com.vb.springboot.controller;
 
 import au.com.vb.springboot.model.Email;
 import au.com.vb.springboot.model.EmailResponse;
-import au.com.vb.springboot.service.impl.MailGunEmailServiceImpl;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import au.com.vb.springboot.service.MailGunEmailServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,25 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmailController {
 
-  @Autowired
-  private MailGunEmailServiceImpl emailService;
-
   @PostMapping(value = "/send-email",
           consumes = MediaType.APPLICATION_JSON_VALUE,
           produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.CREATED)
-  public EmailResponse sendEmail(@RequestBody String emailRequest) {
+  public EmailResponse sendEmail(@RequestBody Email email) {
 
-    EmailResponse response;
-    try {
-      ObjectMapper objectMapper = new ObjectMapper();
-      Email email = objectMapper.readValue(emailRequest, Email.class);
+    MailGunEmailServiceImpl mailGunEmailServiceImpl = new MailGunEmailServiceImpl();
 
-      response = emailService.sendEmail(email);
-    } catch (IOException e) {
-      response = null;
-    }
-    return response;
+    return mailGunEmailServiceImpl.sendEmail(email);
 
   }
 }
